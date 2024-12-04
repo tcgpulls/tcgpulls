29c22e651cd6fa11057aded5248cf192a203769a
Alexandre Champagne <achampagnedev@gmail.com>
2024-12-04 20:46:10 +0700

    Init

---
+++ tcgpulls/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "PokePulls",
+  description: "Get your Pokemon TCG pack pulls and pull rates all in one place!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
