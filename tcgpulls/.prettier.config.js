db1588833a8357e74323e2a3604ce6a7ab73b315
Yannick Bisaillon <ybisaillon@globalia.ca>
2024-12-08 17:11:17 -0500

    Configure docker and vscode settings

--- tcgpulls/.prettier.config.js
+++ tcgpulls/.prettier.config.js
@@ -5,10 +5,10 @@
  * @type {import("prettier").Config}
  */
 const config = {
-  trailingComma: "es5",
-  tabWidth: 4,
+  trailingComma: 'es5',
+  tabWidth: 2,
   semi: false,
   singleQuote: true,
-};
+}
 
-export default config;
+module.exports = config
