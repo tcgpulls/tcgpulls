c172925fef197c6397763c4695f299cc462bf69a
Yannick Bisaillon <ybisaillon@globalia.ca>
2024-12-08 17:12:41 -0500

    sync user with clerk webhooks

---
+++ tcgpulls/prisma/migrations/20241208133625_update_user/migration.sql
@@ -0,0 +1,29 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `emailVerified` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `image` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `name` on the `User` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[clerkUserId]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `clerkUserId` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Made the column `email` on table `User` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Account" DROP CONSTRAINT "Account_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Session" DROP CONSTRAINT "Session_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "emailVerified",
+DROP COLUMN "image",
+DROP COLUMN "name",
+ADD COLUMN     "avatar" TEXT,
+ADD COLUMN     "clerkUserId" TEXT NOT NULL,
+ADD COLUMN     "firstName" TEXT,
+ADD COLUMN     "lastName" TEXT,
+ALTER COLUMN "email" SET NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_clerkUserId_key" ON "User"("clerkUserId");
