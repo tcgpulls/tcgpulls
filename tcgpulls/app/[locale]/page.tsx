828d54e904a912dfa5025c9a9dd84f622436a5a3
Alexandre Champagne <achampagnedev@gmail.com>
2024-12-15 00:08:25 +0700

    Keep calm and keep coding

--- tcgpulls/app/[locale]/page.tsx
+++ tcgpulls/app/[locale]/page.tsx
@@ -2,12 +2,13 @@ import { getTranslations } from "next-intl/server";
 import { Metadata } from "next";
 import { UrlParamsT } from "@/types/Params";
 import { Button } from "@/components/catalyst-ui/button";
+import { Heading } from "@/components/catalyst-ui/heading";
 
 const HomePage = async () => {
   const t = await getTranslations("landing_page");
   return (
     <div>
-      <h1>{t("title")}</h1>
+      <Heading>{t("title")}</Heading>
       <Button href={`/app`}>Go to App</Button>
     </div>
   );
