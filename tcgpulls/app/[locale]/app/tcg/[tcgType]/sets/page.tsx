f3e09862dc567f3df738b8d7f82900abbbd0924c
Alexandre Champagne <achampagnedev@gmail.com>
2024-12-14 14:57:31 +0700

    restoring sets/page.tsx

--- tcgpulls/app/[locale]/app/tcg/[tcgType]/sets/page.tsx
+++ tcgpulls/app/[locale]/app/tcg/[tcgType]/sets/page.tsx
@@ -1,27 +1,37 @@
+import SetCard from "@/components/tcg/SetCard";
 import PageHeader from "@/components/misc/PageHeader";
 import { RectangleStackIcon } from "@heroicons/react/20/solid";
+import PaginationComponent from "@/components/misc/Pagination";
+import { PokemonSet } from "@prisma/client";
 import { getTranslations } from "next-intl/server";
 import { ParamsT } from "@/types/Params";
+import axiosInstance from "@/utils/axiosInstance";
+
+const PAGE_SIZE = 36;
 
 interface Props {
   params: ParamsT;
   searchParams: Promise<{ page?: string }>;
 }
 
-const PokemonTCGPage = async ({}: Props) => {
+const PokemonTCGPage = async ({ params, searchParams }: Props) => {
+  const { tcgType } = await params;
+  const { page } = await searchParams;
   const t = await getTranslations();
+  const currentPage = parseInt(page || "1", 10);
+  const offset = (currentPage - 1) * PAGE_SIZE;
 
-  // const response = await axiosInstance(`/api/public/tcg/${tcgType}/sets`, {
-  //   params: {
-  //     tcg_language: "en",
-  //     limit: PAGE_SIZE,
-  //     offset,
-  //   },
-  // });
-  //
-  // const { data: sets, total } = response.data;
-  //
-  // const totalPages = Math.ceil(total / PAGE_SIZE);
+  const response = await axiosInstance(`/api/public/tcg/${tcgType}/sets`, {
+    params: {
+      tcg_language: "en",
+      limit: PAGE_SIZE,
+      offset,
+    },
+  });
+
+  const { data: sets, total } = response.data;
+
+  const totalPages = Math.ceil(total / PAGE_SIZE);
 
   return (
     <>
@@ -29,16 +39,16 @@ const PokemonTCGPage = async ({}: Props) => {
         title={t("common.tcg_pokemon")}
         icon={<RectangleStackIcon />}
       />
-      {/*<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">*/}
-      {/*  {sets.map((set: PokemonSet) => (*/}
-      {/*    <SetCard*/}
-      {/*      key={set.id}*/}
-      {/*      set={set}*/}
-      {/*      href={`/app/tcg/pokemon/sets/${set.originalId}`}*/}
-      {/*    />*/}
-      {/*  ))}*/}
-      {/*</div>*/}
-      {/*<PaginationComponent currentPage={currentPage} totalPages={totalPages} />*/}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
+        {sets.map((set: PokemonSet) => (
+          <SetCard
+            key={set.id}
+            set={set}
+            href={`/app/tcg/pokemon/sets/${set.originalId}`}
+          />
+        ))}
+      </div>
+      <PaginationComponent currentPage={currentPage} totalPages={totalPages} />
     </>
   );
 };
