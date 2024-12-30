8f1e3d0bcf06c59be0d4f0487cf30b1d47aa7855
Alexandre Champagne <achampagnedev@gmail.com>
2024-12-30 19:23:45 +0700

    switching back to default database URL

--- tcgpulls/lib/prisma.ts
+++ tcgpulls/lib/prisma.ts
@@ -2,20 +2,10 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-const databaseUrl =
-  process.env.NODE_ENV === "production"
-    ? process.env.DATABASE_URL_UNPOOLED
-    : process.env.DATABASE_URL;
-
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
     log: ["query", "info", "warn", "error"],
-    datasources: {
-      db: {
-        url: `${databaseUrl}`,
-      },
-    },
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
